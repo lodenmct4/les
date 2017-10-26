2bab0d3b6cf41092f6e81d5d7640b312f9187bfa
lodeh <lode.hillaert@student.howest.be>
2017-10-26 11:37:23 +0200

    les 10

--- les 10.py
+++ les 10.py
@@ -1,6 +1,19 @@
 def bereken_som(getal1, getal2):
     som= getal1 + getal2
-    return som()
+    return som
 
 def print_som(getal1,getal2):
     print(getal1+getal2)
+
+def is_even (getal):
+    if getal % 2 ==0:
+        return True
+    else : return False
+
+som = bereken_som(4,9)
+print (som)
+print(bereken_som(78,978))
+
+if is_even(78):
+    print ("78 is even.")
+else: print("78 is oneven.")
